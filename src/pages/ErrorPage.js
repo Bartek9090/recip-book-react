a9b510a170479593aa363161500de18d442e34e5
Bartek Bartosik <b.bartek987@gmail.com>
2024-01-28 00:49:13 +0100

    functions-deploy-test

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -7,7 +7,7 @@ export default function ErrorPage() {
     <div className={classes.errorImgBg}>
       <div className={classes.errorContent}>
         <h2>404</h2>
-        <h2>Something went wrong</h2>
+        <h2>Something went wrong!</h2>
         <Link to="/">
           <button className={classes.button}>Back</button>
         </Link>
