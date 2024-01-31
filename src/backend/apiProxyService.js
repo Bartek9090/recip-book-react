445edb37c7447d572fb669a862cb1afa2695a7f4
Bartek Bartosik <b.bartek987@gmail.com>
2024-01-31 22:04:00 +0100

    setting throng

--- src/backend/apiProxyService.js
+++ src/backend/apiProxyService.js
@@ -2,25 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
+const cluster = require("cluster");
+const throng = require("throng");
 
-const api = express();
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-api.use(cors());
-api.use(express.json());
-api.get("/recipesByIngredients", async (req, res) => {
-  const { search } = req.query;
-  console.log("MAIN INQUARIES ");
-  try {
-    const response = await axios.get(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_RECIP_BOOK_KEY}&ingredients=${search}`
-    );
+function start() {
+  const api = express();
 
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error fetching recipes:", error);
-    res.status(500).json({ error: "Error fetching recipes" });
-  }
-});
+  api.use(cors());
+  api.use(express.json());
 
-const PORT = process.env.PORT || 4000;
-api.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+  api.get("/recipesByIngredients", async (req, res) => {
+    const { search } = req.query;
+    console.log("MAIN INQUARIES ");
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_RECIP_BOOK_KEY}&ingredients=${search}`
+      );
+
+      res.json(response.data);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+      res.status(500).json({ error: "Error fetching recipes" });
+    }
+  });
+
+  const PORT = process.env.PORT || 4000;
+  api.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+}
+
+if (cluster.isMaster) {
+  throng(
+    {
+      workers: WORKERS,
+      lifetime: Infinity,
+    },
+    start
+  );
+} else {
+  start();
+}
