29aa27bc1af5dd6e5ca21f17f9d37de9e21c16fe
Bartek Bartosik <b.bartek987@gmail.com>
2024-02-01 23:34:29 +0100

    pushing old backend

--- src/api/apiProxyService.js
+++ src/api/apiProxyService.js
@@ -1,17 +1,26 @@
+const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
+require("dotenv").config();
 
-module.exports = async (req, res) => {
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+api.get("/recipesByIngredients", async (req, res) => {
   const { search } = req.query;
   console.log("MAIN INQUARIES ");
-
   try {
     const response = await axios.get(
       `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_API_RECIP_BOOK_KEY}&ingredients=${search}`
     );
 
-    res.status(200).json(response.data);
+    res.json(response.data);
   } catch (error) {
     console.error("Error fetching recipes:", error);
     res.status(500).json({ error: "Error fetching recipes" });
   }
-};
+});
+
+const PORT = process.env.PORT || 4000;
+api.listen(PORT, () => console.log(`Server is running on ${PORT}`));
